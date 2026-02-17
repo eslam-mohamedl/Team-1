2ea318c426b9c53cbf5420940dd98242973a33ef
eslam-mohamedl <ee.eslam355@gmail.com>
2026-02-17 20:40:05 +0200

    update page home

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,9 @@
-import AboutSection from "@/components/organisms/HeroSection";
+import HeroSection from "@/components/organisms/HeroSection";
+import AboutSection from "@/components/organisms/AboutSection";
 export default function page() {
   return (
     <div>
+      <HeroSection />
       <AboutSection />
     </div>
   );
