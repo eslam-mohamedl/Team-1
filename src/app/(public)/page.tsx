f59eede0a0dea057a82237029a89deefb7f5b74b
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-02-17 21:57:02 +0300

    feat: add NavbarSection Componetes

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import AboutSection from "@/components/organisms/AboutSection";
+import NavbarSection from "@/components/organisms/NavbarSection";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <NavbarSection />
     </div>
   );
 }
