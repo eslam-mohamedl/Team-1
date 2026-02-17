a5a8bd998f99f6e4815307d9e070084e84217dd8
MohamedNawsany <dnawasany@gmail.com>
2026-02-17 20:52:13 +0200

    feat: add feature component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HeroSection";
 import AboutSection from "@/components/organisms/AboutSection";
+import FeatureSection from "@/components/organisms/FeatureSection";
 export default function page() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <FeatureSection />
     </div>
   );
 }
